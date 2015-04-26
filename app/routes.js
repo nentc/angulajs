c388caba30aab5f29f933b5d5e4b7db6e90e1f65
unknown <goncalomarques101@gmail.com>
2015-04-26 20:06:32 +0100

    Add routes file

---
+++ app/routes.js
@@ -0,0 +1,16 @@
+angular.module('workshopApp')
+    .config(function() {
+        // $routeProvider
+        //     .when('/main', {
+        //         templateUrl: 'main.html',
+        //         controller: 'MainController'
+        //     });
+
+
+        // $stateProvider
+        //     .state('main', {
+        //         url: '/main',
+        //         template: 'main.html',
+        //         controller: 'MainController'
+        //     });
+    });
